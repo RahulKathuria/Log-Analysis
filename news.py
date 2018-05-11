97ff54fc15fed4b997b5f2360ea22e41c3fd56e2
RahulKathuria <rahulkathuria52@gmail.com>
2018-05-12 01:46:48 +0530

    Completed three queries

--- news.py
+++ news.py
@@ -27,14 +27,67 @@ query2 = ["""Who are the most popular article authors of all time?"""]
 query3 = \
     ["""On which days did more than 1% of requests lead to errors?"""]
 
-query1_solution = ("""select articles.title as title, count(*) as total 
-                      from articles, log where log.path = '/article/' || articles.slug 
-                      group by articles.title order by total desc limit 3;""")
-query2_solution = ("""select authors.name , count(*) as total from authors, 
-            articles, log where articles.author = authors.id and 
-            articles.slug  = substr(log.path,10) group by authors.name 
-            order by total desc""")
-query3_solution = (""" """)
+query1_solution = ("""
+            SELECT 
+                articles.title as title,
+                count(*) as total 
+            FROM 
+                articles, log 
+            WHERE 
+                log.path = '/article/' || articles.slug 
+            GROUP BY 
+                articles.title 
+            ORDER BY 
+                total desc limit 3;""")
+query2_solution = ("""
+            SELECT 
+                authors.name,
+                count(*) as total
+            FROM 
+                authors, 
+                articles,
+                log 
+            WHERE
+                articles.author = authors.id
+            AND 
+                articles.slug  = substr(log.path,10)
+            GROUP BY 
+                authors.name 
+            ORDER BY total desc""")
+query3_solution = ("""
+        SELECT 
+            *
+        FROM 
+            (SELECT  
+                err.date,
+                round(100 * err.e / requests.r,2) as p
+            FROM
+                (SELECT 
+                    date(time) as date, 
+                    count(status) as e
+                FROM 
+                    log
+                WHERE 
+                    status != '200 OK'
+                GROUP BY
+                    date 
+                ORDER BY
+                    date) err,
+                (SELECT
+                    date(time) as date, 
+                    count(status) as r
+                FROM 
+                    log
+                GROUP BY
+                    date
+                ) requests
+            WHERE
+                err.date = requests.date) analysis
 
+        WHERE 
+            p > 1.00
+            """)
+print_query(query1,query1_solution)
 print_query(query2,query2_solution)
-			
+print_query(query3,query3_solution)
+            
